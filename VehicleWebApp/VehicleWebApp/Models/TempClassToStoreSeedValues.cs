10fabc0f888fbdda3e4c5d8098dfa6ed68f72ffe
Gaffrs <colmgaffney@yahoo.ie>
2017-03-17 21:10:57 +0000

    changed to test github9

--- VehicleWebApp/VehicleWebApp/Models/TempClassToStoreSeedValues.cs
+++ VehicleWebApp/VehicleWebApp/Models/TempClassToStoreSeedValues.cs
@@ -7,7 +7,7 @@ namespace VehicleWebApp.Models
 {
     public class TempClassToStoreSeedValues
     {
-        //changed to test github8
+        //changed to test github9
         //Collection in Memory, list
         List<Vehicle> Vehicles;         //<Vehicle> is the name of the Class in Model
         List<User> Users;
