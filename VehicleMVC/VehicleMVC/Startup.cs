2ca573b4c8bdb22e15a14cd8a1b1ebcb7e08d2e6
Gaffrs <colmgaffney@yahoo.ie>
2017-04-16 07:50:44 +0100

    Starting MVC project

---
+++ VehicleMVC/VehicleMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VehicleMVC.Startup))]
+namespace VehicleMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
