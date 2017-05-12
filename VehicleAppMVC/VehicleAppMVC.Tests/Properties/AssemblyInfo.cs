314d9537a9ff836b742e93c9609a94f0fa409bfd
Gaffrs <colmgaffney@yahoo.ie>
2017-05-12 17:39:04 +0100

    Calidation checks added to Fuels (Create & Edit) for Date & Odo

--- VehicleAppMVC/VehicleAppMVC.Tests/Properties/AssemblyInfo.cs
+++ VehicleAppMVC/VehicleAppMVC.Tests/Properties/AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VehicleMVC.Tests")]
+[assembly: AssemblyTitle("VehicleAppMVC.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VehicleMVC.Tests")]
+[assembly: AssemblyProduct("VehicleAppMVC.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6f0f0b82-80ed-4ec4-aa99-e79bdc2a6b3e")]
+[assembly: Guid("83b7f18a-7d82-4df7-a234-062509f5e69e")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VehicleAppMVC/VehicleAppMVC.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VehicleAppMVC.Controllers;
+
+namespace VehicleAppMVC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FuelConsumptionCalcullation()
+        {
+            var controller = new FuelsController { };
+        }
+    }
+}
