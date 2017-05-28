31bc5b37a1fc22db73b5284d17ae85809dc78b40
Gaffrs <colmgaffney@yahoo.ie>
2017-05-28 14:06:02 +0100

    Calculations ammended for L/100KM to Lx100/Km

--- VehicleAppMVC/VehicleAppMVC/Models/TempClassToStoreSeedValues.cs
+++ VehicleAppMVC/VehicleAppMVC/Models/TempClassToStoreSeedValues.cs
@@ -81,66 +81,66 @@ namespace VehicleAppMVC.Models
 
 
             //*********************************************************************
-/*
-            //Setting up seed data from Scratch
-            //Delete ALL Tables
-            //1)Register User Paul
-            //2)Register User Colm
+            /*
+                        //Setting up seed data from Scratch
+                        //Delete ALL Tables
+                        //1)Register User Paul
+                        //2)Register User Colm
 
-/*
+            /*
 
-            //Create a list of Vehicles (x2 Users)
+                        //Create a list of Vehicles (x2 Users)
 
-                context.Vehicles.AddOrUpdate(v => v.VehicleID,
-                   new Vehicle { ApplicationUserId = "8ad230ab-0758-4066-952b-e0f163c648f4", VehicleMake = "Paul Subaru", VehicleModel = "Paul Impreza", VehicleRegistrationNumber = "Paul1", VehicleOdometerMileage = 500, SettingFuelType = eSettingFuelType.Petrol, SettingDistance = eSettingDistance.Miles, SettingVolume = eSettingVolume.UK_Gal, SettingConsumption = eSettingConsumption.UK_Mpg },  //User Paul 
-                   new Vehicle { ApplicationUserId = "8ad230ab-0758-4066-952b-e0f163c648f4", VehicleMake = "Paul Volkswagen", VehicleModel = "Paul Polo", VehicleRegistrationNumber = "Paul2", VehicleOdometerMileage = 1500, SettingFuelType = eSettingFuelType.Petrol, SettingDistance = eSettingDistance.Miles, SettingVolume = eSettingVolume.US_Gal, SettingConsumption = eSettingConsumption.US_Mpg }, //User Paul 
-                   new Vehicle { ApplicationUserId = "dde3844d-46cf-49d7-80a9-5b60f22b4697", VehicleMake = "Colm Opel", VehicleModel = "Colm Vectra", VehicleRegistrationNumber = "Colm1", VehicleOdometerMileage = 1000, SettingFuelType = eSettingFuelType.Petrol, SettingDistance = eSettingDistance.Km, SettingVolume = eSettingVolume.Litres, SettingConsumption = eSettingConsumption.Lper100km },     //User Colm 
-                   new Vehicle { ApplicationUserId = "dde3844d-46cf-49d7-80a9-5b60f22b4697", VehicleMake = "Colm Mercedes", VehicleModel = "Colm C-Class", VehicleRegistrationNumber = "Colm2", VehicleOdometerMileage = 1300, SettingFuelType = eSettingFuelType.Diesel, SettingDistance = eSettingDistance.Km, SettingVolume = eSettingVolume.Litres, SettingConsumption = eSettingConsumption.Lper100km } //User Colm 
-                 );
-
-
-
-            //Create a list of Notifications (x2 Users)
+                            context.Vehicles.AddOrUpdate(v => v.VehicleID,
+                               new Vehicle { ApplicationUserId = "8ad230ab-0758-4066-952b-e0f163c648f4", VehicleMake = "Paul Subaru", VehicleModel = "Paul Impreza", VehicleRegistrationNumber = "Paul1", VehicleOdometerMileage = 500, SettingFuelType = eSettingFuelType.Petrol, SettingDistance = eSettingDistance.Miles, SettingVolume = eSettingVolume.UK_Gal, SettingConsumption = eSettingConsumption.UK_Mpg },  //User Paul 
+                               new Vehicle { ApplicationUserId = "8ad230ab-0758-4066-952b-e0f163c648f4", VehicleMake = "Paul Volkswagen", VehicleModel = "Paul Polo", VehicleRegistrationNumber = "Paul2", VehicleOdometerMileage = 1500, SettingFuelType = eSettingFuelType.Petrol, SettingDistance = eSettingDistance.Miles, SettingVolume = eSettingVolume.US_Gal, SettingConsumption = eSettingConsumption.US_Mpg }, //User Paul 
+                               new Vehicle { ApplicationUserId = "dde3844d-46cf-49d7-80a9-5b60f22b4697", VehicleMake = "Colm Opel", VehicleModel = "Colm Vectra", VehicleRegistrationNumber = "Colm1", VehicleOdometerMileage = 1000, SettingFuelType = eSettingFuelType.Petrol, SettingDistance = eSettingDistance.Km, SettingVolume = eSettingVolume.Litres, SettingConsumption = eSettingConsumption.Lper100km },     //User Colm 
+                               new Vehicle { ApplicationUserId = "dde3844d-46cf-49d7-80a9-5b60f22b4697", VehicleMake = "Colm Mercedes", VehicleModel = "Colm C-Class", VehicleRegistrationNumber = "Colm2", VehicleOdometerMileage = 1300, SettingFuelType = eSettingFuelType.Diesel, SettingDistance = eSettingDistance.Km, SettingVolume = eSettingVolume.Litres, SettingConsumption = eSettingConsumption.Lper100km } //User Colm 
+                             );
 
 
-                context.Notifications.AddOrUpdate(n => n.NotificationID,
-                     new Notification { ApplicationUserId = "8ad230ab-0758-4066-952b-e0f163c648f4", NotificationDate = new DateTime(2018, 2, 28), NotificationSendDate = new DateTime(2018, 2, 20), NotificationType = eNotificationType.Email, NotificationTitle = Notification.eNotificationTitle.InsuranceDateRenewal },        //User Paul
-                     new Notification { ApplicationUserId = "8ad230ab-0758-4066-952b-e0f163c648f4", NotificationDate = new DateTime(2018, 10, 01), NotificationSendDate = new DateTime(2018, 09, 20), NotificationType = eNotificationType.SMS, NotificationTitle = Notification.eNotificationTitle.MotorTaxDateRenewal },           //User Paul
-                     new Notification { ApplicationUserId = "dde3844d-46cf-49d7-80a9-5b60f22b4697", NotificationDate = new DateTime(2018, 7, 01), NotificationSendDate = new DateTime(2018, 1, 01), NotificationType = eNotificationType.Email, NotificationTitle = Notification.eNotificationTitle.ServiceDateNotification },     //User Colm 
-                     new Notification { ApplicationUserId = "dde3844d-46cf-49d7-80a9-5b60f22b4697", NotificationDate = new DateTime(2018, 7, 01), NotificationSendDate = new DateTime(2018, 1, 01), NotificationType = eNotificationType.Email, NotificationTitle = Notification.eNotificationTitle.ServiceMileageNotification }   //User Colm 
-                 );
 
-            //Create a list of Costs
-
-            context.Costs.AddOrUpdate(c => c.CostID,
-            new Cost { VehicleID = 1, CostDate = new DateTime(2017, 3, 01), CostOdometerMileage = 500, CostTitle = eCostTitle.Insurance, CostRunningCost = 700 },   //User Paul Vehicle 1
-            new Cost { VehicleID = 1, CostDate = new DateTime(2017, 8, 01), CostOdometerMileage = 3000, CostTitle = eCostTitle.Service, CostRunningCost = 400 },    //User Paul Vehicle 1
-            new Cost { VehicleID = 1, CostDate = new DateTime(2017, 10, 01), CostOdometerMileage = 4000, CostTitle = eCostTitle.Tax, CostRunningCost = 250 },       //User Paul Vehicle 1
-            new Cost { VehicleID = 2, CostDate = new DateTime(2017, 3, 01), CostOdometerMileage = 1500, CostTitle = eCostTitle.Insurance, CostRunningCost = 1500 },   //User Paul Vehicle 2
-            new Cost { VehicleID = 2, CostDate = new DateTime(2017, 8, 01), CostOdometerMileage = 4000, CostTitle = eCostTitle.Service, CostRunningCost = 1000 },    //User Paul Vehicle 2
-            new Cost { VehicleID = 2, CostDate = new DateTime(2017, 10, 01), CostOdometerMileage = 5000, CostTitle = eCostTitle.Tax, CostRunningCost = 1200 },       //User Paul Vehicle 2
-            new Cost { VehicleID = 3, CostDate = new DateTime(2017, 4, 01), CostOdometerMileage = 2500, CostTitle = eCostTitle.Insurance, CostRunningCost = 400 },   //User Colm Vehicle 3
-            new Cost { VehicleID = 3, CostDate = new DateTime(2017, 9, 01), CostOdometerMileage = 5000, CostTitle = eCostTitle.Service, CostRunningCost = 1000 },    //User Colm Vehicle 3
-            new Cost { VehicleID = 3, CostDate = new DateTime(2017, 11, 01), CostOdometerMileage = 6000, CostTitle = eCostTitle.Tax, CostRunningCost = 500 }        //User Colm Vehicle 3
-            );
+                        //Create a list of Notifications (x2 Users)
 
 
-            //Create a list of Fuel fills
+                            context.Notifications.AddOrUpdate(n => n.NotificationID,
+                                 new Notification { ApplicationUserId = "8ad230ab-0758-4066-952b-e0f163c648f4", NotificationDate = new DateTime(2018, 2, 28), NotificationSendDate = new DateTime(2018, 2, 20), NotificationType = eNotificationType.Email, NotificationTitle = Notification.eNotificationTitle.InsuranceDateRenewal },        //User Paul
+                                 new Notification { ApplicationUserId = "8ad230ab-0758-4066-952b-e0f163c648f4", NotificationDate = new DateTime(2018, 10, 01), NotificationSendDate = new DateTime(2018, 09, 20), NotificationType = eNotificationType.SMS, NotificationTitle = Notification.eNotificationTitle.MotorTaxDateRenewal },           //User Paul
+                                 new Notification { ApplicationUserId = "dde3844d-46cf-49d7-80a9-5b60f22b4697", NotificationDate = new DateTime(2018, 7, 01), NotificationSendDate = new DateTime(2018, 1, 01), NotificationType = eNotificationType.Email, NotificationTitle = Notification.eNotificationTitle.ServiceDateNotification },     //User Colm 
+                                 new Notification { ApplicationUserId = "dde3844d-46cf-49d7-80a9-5b60f22b4697", NotificationDate = new DateTime(2018, 7, 01), NotificationSendDate = new DateTime(2018, 1, 01), NotificationType = eNotificationType.Email, NotificationTitle = Notification.eNotificationTitle.ServiceMileageNotification }   //User Colm 
+                             );
 
-            context.Fuels.AddOrUpdate(f => f.FuelID,
-            new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 3, 01), FuelOdometerMileage = 500, FuelQuantity = 45, FuelUnitPrice = 1.35, FuelPartialFill = false },
-            new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 3, 10), FuelOdometerMileage = 1000, FuelQuantity = 40, FuelUnitPrice = 1.45, FuelPartialFill = false },
-            new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 3, 24), FuelOdometerMileage = 1600, FuelQuantity = 48, FuelUnitPrice = 1.55, FuelPartialFill = false },
-            new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 3, 29), FuelOdometerMileage = 2000, FuelQuantity = 50, FuelUnitPrice = 1.25, FuelPartialFill = false },
-            new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 4, 05), FuelOdometerMileage = 2500, FuelQuantity = 55, FuelUnitPrice = 1.15, FuelPartialFill = false },
-            new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 4, 01), FuelOdometerMileage = 1500, FuelQuantity = 45, FuelUnitPrice = 1.35, FuelPartialFill = false },
-            new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 4, 10), FuelOdometerMileage = 2000, FuelQuantity = 40, FuelUnitPrice = 1.45, FuelPartialFill = false },
-            new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 4, 24), FuelOdometerMileage = 2100, FuelQuantity = 48, FuelUnitPrice = 1.55, FuelPartialFill = false },
-            new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 4, 29), FuelOdometerMileage = 2500, FuelQuantity = 50, FuelUnitPrice = 1.25, FuelPartialFill = false },
-            new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 5, 05), FuelOdometerMileage = 3000, FuelQuantity = 55, FuelUnitPrice = 1.15, FuelPartialFill = false },
-            new Fuel { VehicleID = 3, FuelDate = new DateTime(2017, 5, 05), FuelOdometerMileage = 2500, FuelQuantity = 35, FuelUnitPrice = 1.15, FuelPartialFill = false }
-        );
-*/
+                        //Create a list of Costs
+
+                        context.Costs.AddOrUpdate(c => c.CostID,
+                        new Cost { VehicleID = 1, CostDate = new DateTime(2017, 3, 01), CostOdometerMileage = 500, CostTitle = eCostTitle.Insurance, CostRunningCost = 700 },   //User Paul Vehicle 1
+                        new Cost { VehicleID = 1, CostDate = new DateTime(2017, 8, 01), CostOdometerMileage = 3000, CostTitle = eCostTitle.Service, CostRunningCost = 400 },    //User Paul Vehicle 1
+                        new Cost { VehicleID = 1, CostDate = new DateTime(2017, 10, 01), CostOdometerMileage = 4000, CostTitle = eCostTitle.Tax, CostRunningCost = 250 },       //User Paul Vehicle 1
+                        new Cost { VehicleID = 2, CostDate = new DateTime(2017, 3, 01), CostOdometerMileage = 1500, CostTitle = eCostTitle.Insurance, CostRunningCost = 1500 },   //User Paul Vehicle 2
+                        new Cost { VehicleID = 2, CostDate = new DateTime(2017, 8, 01), CostOdometerMileage = 4000, CostTitle = eCostTitle.Service, CostRunningCost = 1000 },    //User Paul Vehicle 2
+                        new Cost { VehicleID = 2, CostDate = new DateTime(2017, 10, 01), CostOdometerMileage = 5000, CostTitle = eCostTitle.Tax, CostRunningCost = 1200 },       //User Paul Vehicle 2
+                        new Cost { VehicleID = 3, CostDate = new DateTime(2017, 4, 01), CostOdometerMileage = 2500, CostTitle = eCostTitle.Insurance, CostRunningCost = 400 },   //User Colm Vehicle 3
+                        new Cost { VehicleID = 3, CostDate = new DateTime(2017, 9, 01), CostOdometerMileage = 5000, CostTitle = eCostTitle.Service, CostRunningCost = 1000 },    //User Colm Vehicle 3
+                        new Cost { VehicleID = 3, CostDate = new DateTime(2017, 11, 01), CostOdometerMileage = 6000, CostTitle = eCostTitle.Tax, CostRunningCost = 500 }        //User Colm Vehicle 3
+                        );
+
+
+                        //Create a list of Fuel fills
+
+                        context.Fuels.AddOrUpdate(f => f.FuelID,
+                        new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 3, 01), FuelOdometerMileage = 800,  FuelQuantity = 35, FuelUnitPrice = 1.35, FuelPartialFill = false },
+                        new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 3, 10), FuelOdometerMileage = 1800, FuelQuantity = 40, FuelUnitPrice = 1.45, FuelPartialFill = false },
+                        new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 3, 24), FuelOdometerMileage = 2550, FuelQuantity = 29, FuelUnitPrice = 1.55, FuelPartialFill = false },
+                        new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 3, 29), FuelOdometerMileage = 3550, FuelQuantity = 35, FuelUnitPrice = 1.25, FuelPartialFill = false },
+                        new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 4, 05), FuelOdometerMileage = 4250, FuelQuantity = 30, FuelUnitPrice = 1.15, FuelPartialFill = false },
+                        new Fuel { VehicleID = 1, FuelDate = new DateTime(2017, 5, 05), FuelOdometerMileage = 4800, FuelQuantity = 25, FuelUnitPrice = 1.15, FuelPartialFill = false },
+                        new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 4, 01), FuelOdometerMileage = 1500, FuelQuantity = 45, FuelUnitPrice = 1.35, FuelPartialFill = false },
+                        new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 4, 10), FuelOdometerMileage = 2500, FuelQuantity = 35, FuelUnitPrice = 1.45, FuelPartialFill = false },
+                        new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 4, 24), FuelOdometerMileage = 3000, FuelQuantity = 18, FuelUnitPrice = 1.55, FuelPartialFill = false },
+                        new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 4, 29), FuelOdometerMileage = 4000, FuelQuantity = 40, FuelUnitPrice = 1.25, FuelPartialFill = false },
+                        new Fuel { VehicleID = 2, FuelDate = new DateTime(2017, 5, 05), FuelOdometerMileage = 4700, FuelQuantity = 25, FuelUnitPrice = 1.15, FuelPartialFill = false }
+                    );
+            */
 
         }
     }
